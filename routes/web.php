e94887255493d206a64a65a563a2a583ada78c16
Tapas Datta <tapash.me@gmail.com>
2017-07-21 20:18:37 +0600

    appropiate route

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,18 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+/**
+ * videos route except create, edit
+ * @link(https://laravel.com/docs/5.4/controllers#resource-controllers)
+ */
+Route::resource('videos', 'VideoController',[ 'except' => [
+	'create', 'edit'
+]]);
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::resource('channels', 'ChannelController',[ 'except' => [
+	'create', 'edit'
+]]);
+
+Route::resource('commnets', 'CommentController',[ 'except' => [
+	'create', 'edit'
+]]);
